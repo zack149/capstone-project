27cda9abbcab13d76174a53abe540a56536765d5
zack149 <89365572+zack149@users.noreply.github.com>
2021-10-27 11:51:55 -0500

    Initial project files

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+Adam Silver and Zack Jordan
+"Settling Down and Setting Up Shop: Using Machine Learning to Help Shape Our Neighborhoods"
+
+Version 0.1: Milestone 2 Demo
+'''
+
+#TODO: command-line input of user input, import sample data set, implement basic algorithm
+# No ML yet, just using weights and equations from FCLA paper.
+
+
