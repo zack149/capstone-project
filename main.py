5171b4e2bbebef7f9ce1b7d05277e57610e97afc
zack149 <89365572+zack149@users.noreply.github.com>
2021-10-28 17:26:42 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-'''
-Adam Silver and Zack Jordan
-"Settling Down and Setting Up Shop: Using Machine Learning to Help Shape Our Neighborhoods"
+#Adam Silver and Zack Jordan
+#"Settling Down and Setting Up Shop: Using Machine Learning to Help Shape Our Neighborhoods"
+#
+#Version 0.1: Milestone 2 Demo
 
-Version 0.1: Milestone 2 Demo
-'''
+import numpy as np
+
+f_in = open("input.txt", "r")
+f_data = open("data.txt", "r")
+
+
 
 #TODO: command-line input of user input, import sample data set, implement basic algorithm
 # No ML yet, just using weights and equations from FCLA paper.
